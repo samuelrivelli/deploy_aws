8a3086932191a3928de4e322b3c0b7ca10d1cea6
Samuel Rivelli <samuelmagrivelli@gmail.com>
2023-09-04 16:41:07 -0300

    teste att

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ app.get('/', (request,response) =>{
     return response.json({message:'Server is up'})
 })
 
+app.get('/atualizou', (request,response) =>{
+    return response.json({message:'Atualizou'})
+})
+
 app.listen(3333)
